e5f5e399f60f2a0f12c582f9ffdb2325591ca585
Jakub Surdej <jakub.surdej@microsoft.wsei.edu.pl>
2022-07-14 22:42:11 +0200

    Generate migrations for new models

--- models/ReservationGuest.cs
+++ models/ReservationGuest.cs
@@ -6,9 +6,11 @@ namespace Models
     {
         public int Id { get; set; }
 
+        public int ReservationId { get; set; }
         [Required(ErrorMessage = "Please fill information about the Reservation")]
         public Reservation Reservation { get; set; }
 
+        public int ClientId { get; set; }
         [Required(ErrorMessage = "Please fill information about Client")]
         public Client Client { get; set; }
     }
