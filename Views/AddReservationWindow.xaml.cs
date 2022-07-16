446eb90fbaae6a2fc18da49f290c992847b42417
Jakub Surdej <jakub.surdej@microsoft.wsei.edu.pl>
2022-07-16 15:44:53 +0200

    Add layout for adding reservation

--- Views/AddReservationWindow.xaml.cs
+++ Views/AddReservationWindow.xaml.cs
@@ -1,6 +1,11 @@
 using System.Windows;
 using Microsoft.Extensions.Logging;
 using Utils;
+using Models;
+using Repositories;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
 
 namespace Views
 {
@@ -12,15 +17,48 @@ namespace Views
         {
             logger.LogInformation("Initializing");
             InitializeComponent();
+            SetupRoomsDropdown();
+            SetupClientsDropdown();
         }
 
-        private void SetupRoomsDropdown()
+        private async void SetupRoomsDropdown()
         {
+            RoomsCombo.DataContext = await GetRooms();
+        }
+
+        private async void SetupClientsDropdown()
+        {
+            ClientsLB.DataContext = await GetClients();
         }
 
         private void AddReservationSubmit(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private async Task<ObservableCollection<int>> GetRooms()
+        {
+            using (var repo = new RoomsRepository())
+            {
+                var rooms = await repo.GetAllRooms();
+                var roomIds = new List<int>();
+
+                foreach (Room room in rooms)
+                {
+                    roomIds.Add(room.Id);
+                }
+
+                return new ObservableCollection<int>(roomIds);
+            }
+        }
+
+        private async Task<ObservableCollection<Client>> GetClients()
+        {
+            using (var repo = new ClientsRepository())
+            {
+                var clients = await repo.GetAllClients();
+                return new ObservableCollection<Client>(clients);
+            }
         }
     }
 }
